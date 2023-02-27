9452d64ab8b34ea35f7e58e3b4a13055c0d18363
Stuart <stuart.dickenson@lloydsbanking.com>
2023-02-27 12:10:00 +0000

    Add files via upload

---
+++ 05/demos/after/js-demo/home.js
@@ -0,0 +1,29 @@
+
+showMessage('Get a grip!');
+alert("wheres my chocolate gone?")
+
+//console.group("i can do this");
+  
+changePercentOff ( 80 );
+
+const header = document.getElementById('message');
+
+header.style.color = "green";
+/*
+const button = document.getElementById('see-review');
+
+button.addEventListener('click', function() {
+
+  const review = document.getElementById('review');
+
+  if (review.classlist.contains('d-none')) {
+    review.classlist.remove('d-none');
+  }
+else { review.classlist.add('d-none');
+}
+
+});
+*/
+const values = ['a', 'b', 'c'];
+
+console.log(values);
